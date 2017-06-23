02dd00d3f337f02d6d074cf31b1d0b1bb7231651
Ol Kovalenko <avk013@gmail.com>
2017-06-24 00:30:05 +0300

    заплатка после обновления
    заплатка после обновления едбо системы, используется всего 7 полей, остальные статичны

---
+++ edbo_convert_diploma/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace edbo_convert_diploma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Location = new System.Drawing.Point(3, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(394, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "конвертувати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 198);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(202, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Коваленко Олександр (c) avk013";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 239);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "модифікатор формату ЕДБО для дипломів";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ edbo_convert_diploma/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace edbo_convert_diploma
+{
+    public partial class Form1 : Form
+    {
+        string file_path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] in_words = { "розклад", "деканфак", "занятьфак", "______" };
+            string[] out_words = { "num", "lastname", "name", "sex", "lastnameen", "nameen" };
+            string data = @"<?xml version=""1.0"" encoding=""utf-8""?><documents>";
+            // open file
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|*.xml";
+            openFileDialog1.Title = "файл для обработки";
+            openFileDialog1.FileName = "in.xml";
+            openFileDialog1.InitialDirectory = Environment.SpecialFolder.Desktop.ToString();
+            string path = file_path + "Order_2.xml";
+            //file_path= file_path + "2.xml";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+                file_path = Path.GetDirectoryName(path);
+                file_path = file_path + "\\2.xml";
+                //                string[] tab0 = File.ReadAllLines(path, Encoding.Default);
+                //////////////////
+               // textBox1.Text += path;
+                String xmlString = File.ReadAllText(path);
+                XDocument xdoc = XDocument.Load(new StringReader(xmlString));
+
+                var xmlList = (from article in xdoc.Descendants("Document")
+                               select new
+                               {
+                                   series = article.Descendants("DocumentSeries").SingleOrDefault(),
+                                   number = article.Descendants("DocumentNumber").SingleOrDefault(),
+                                   lastname = article.Descendants("LastName").SingleOrDefault(),
+                                   firstname = article.Descendants("FirstName").SingleOrDefault(),
+                                   middlename = article.Descendants("MiddleName").SingleOrDefault(),
+                                   lastnameen = article.Descendants("LastNameEn").SingleOrDefault(),
+                                   firstnameen = article.Descendants("FirstNameEn").SingleOrDefault(),
+                                   idtypesex = article.Descendants("SexId").SingleOrDefault()
+                                   //firstnameen = article.Descendants("FirstNameEn").SingleOrDefault()
+                                   //firstnameen = article.Descendants("FirstNameEn").SingleOrDefault()
+                                   //firstnameen = article.Descendants("FirstNameEn").SingleOrDefault()
+                               }).ToList();
+
+                var articleList = (from item in xmlList
+                                   select new
+                                   {
+                                       series = item.series != null ? item.series.Value : null,
+                                       number = item.number != null ? item.number.Value : null,
+                                       lastname = item.lastname != null ? item.lastname.Value : null,
+                                       firstname = item.firstname != null ? item.firstname.Value : null,
+                                       middlename = item.middlename != null ? item.middlename.Value : null,
+                                       lastnameen = item.lastnameen != null ? item.lastnameen.Value : null,
+                                       firstnameen = item.firstnameen != null ? item.firstnameen.Value : null,
+                                       idtypesex = item.idtypesex != null ? item.idtypesex.Value : null,
+                                       //id = item.series != null ? item.series.Value : null,
+                                       //id = item.series != null ? item.series.Value : null,
+                                       //photo_s = item.photo_s != null ? item.photo_s.Value : null,
+                                       //photo_m = item.photo_m != null ? item.photo_m.Value : null,
+                                       //photo_l = item.photo_l != null ? item.photo_l.Value : null,
+                                       //date = item.date != null ? item.date.Value : null
+                                   });
+
+                foreach (var article in articleList)
+                {
+                  //  textBox1.Text += article.series + article.number+article.lastname+article.firstname+article.middlename+article.lastnameen+article.firstnameen+"\r\n";
+                    data += "<document>";
+                    //                    data += "series=\""+article.series+"\" number=\""+article.number+"\" lastname=\"Якименко\" firstname=\"Надія\" middlename=\"Сергіївна\" fio=\"Якименко Надія Сергіївна\" lastnameen=\"Yakymenko\" firstnameen=\"Nadiia\" middlenameEn=\"Serhiivna\" fioen=\"Yakymenko Nadiia Serhiivna\" idtypesex=\"2\"";
+                    data += @"
+<documentdata universitynamecreate=""Ізмаїльський державний гуманітарний університет"" idorderofdocumente=""214184"" datecreate=""2017 - 02 - 08T17: 36:15.163"" iddocument=""19034036"" idordertype=""1"" type=""Диплом магістра державного зразка"" idpersoneducation=""4985493"" facultetfullname=""Педагогічний факультет"" educationformname=""Денна"" 
+paymenttype=""бюджет"" series=""" + article.series + @""" 
+number=""" + article.number + @""" 
+lastname="""+article.lastname+@""" 
+firstname="""+article.firstname+@""" 
+middlename="""+article.middlename+@""" 
+fio=""Коваленко Олександр Вікторович"" 
+lastnameen=""" + article.lastnameen + @""" 
+firstnameen=""" + article.firstnameen + @""" 
+middlenameEn=""" + article.middlename + @""" 
+fioen=""avk013"" 
+idtypesex=""" + article.idtypesex + @"""
+
+ birthday=""21 / 04 / 1993"" 
+inn=""161316613"" pasporttype=""3"" pasportseries=""KK"" 
+spasportnumber=""13131313"" 
+dateendeducation=""01 / 01 / 2017"" 
+universityname=""Ізмаїльський державний гуманітарний університет"" 
+universitynameen=""IZMAIL STATE  UNIVERSITY OF HUMANITIES"" iduniversity=""89"" 
+qualificationname=""Магістр"" qualificationnameen=""А master`s degree"" 
+specdirprofcode=""8.01010201"" specdirprofname=""початкова освіта"" 
+specdirprofnameen=""Primary education"" specializationname=""Передшкільна освіта"" 
+educationqualificationname=""Магістр початкової освіти"" 
+educationqualificationnameen=""Master of  Primary Education"" 
+educationqualificationnamerod=""Магістра початкової освіти"" 
+bossworkpost=""Ректор"" bossworkposten=""Rector"" boss=""Я.В. Кічук"" 
+bossen=""Yaroslav Kichuk"" dategive=""28/02/2017"" award=""3"" prevdoctype=""11"" 
+prevdocseries=""B15"" prevdocnumber=""134713"" />";
+
+                    data += "</document>";
+                }
+                data += "</documents>";
+                ////////////////
+             //   data = "";
+               // data = @"<?xml version=""1.0"" encoding=""utf-8""?>
+ //<documents><document><documentdata universitynamecreate = ""Ізмаїльський державний гуманітарний університет"" idorderofdocumente = ""214184"" datecreate = ""2017 - 02 - 08T17: 36:15.163"" iddocument = ""19034036"" idordertype = ""1"" type = ""Диплом магістра державного зразка"" idpersoneducation = ""4985493"" facultetfullname = ""Педагогічний факультет"" educationformname = ""Денна"" paymenttype = ""бюджет"" series = ""M17"" number = ""034055"" lastname = ""Якименко"" firstname = ""Надія"" middlename = ""Сергіївна"" fio = ""Якименко Надія Сергіївна"" lastnameen = ""Yakymenko"" firstnameen = ""Nadiia"" middlenameEn = ""Serhiivna"" fioen = ""Yakymenko Nadiia Serhiivna"" idtypesex = ""2"" birthday = ""21 / 04 / 1993"" inn = ""3407912162"" pasporttype = ""3"" pasportseries = ""KM"" pasportnumber = ""604725"" dateendeducation = ""28 / 02 / 2017"" universityname = ""Ізмаїльський державний гуманітарний університет"" universitynameen = ""IZMAIL STATE  UNIVERSITY OF HUMANITIES"" iduniversity = ""89"" qualificationname = ""Магістр"" qualificationnameen = ""А master`s degree"" specdirprofcode = ""8.01010201"" specdirprofname = ""початкова освіта"" specdirprofnameen = ""Primary education"" specializationname = ""Передшкільна освіта"" educationqualificationname = ""Магістр початкової освіти, викладач &#xA;педагогіки та методики початкової освіти, &#xA;організатор передшкільної та початкової освіти"" educationqualificationnameen=""Master of  Primary Education, Teacher of Pedagogy and Methods of  Primary Education, Organizer of Preschool and  Primary Education"" educationqualificationnamerod=""Магістра початкової освіти, викладача &#xA;педагогіки та методики початкової освіти, &#xA;організатора передшкільної та початкової освіти"" bossworkpost=""Ректор"" bossworkposten=""Rector"" boss=""Я.В. Кічук"" bossen=""Yaroslav Kichuk"" dategive=""28/02/2017"" award=""3"" prevdoctype=""11"" prevdocseries=""B15"" prevdocnumber=""134713"" /></document></documents>";
+                Encoding utf8WithoutBom = new UTF8Encoding(false);
+
+                File.WriteAllText(path+"+MOD.xml", data, utf8WithoutBom);
+                textBox1.Text += path+"\r\n";
+
+
+            }
+
+        }
+    }
+}
